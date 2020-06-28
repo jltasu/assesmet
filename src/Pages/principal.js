6aa629a655c6dcd6069213825068d702688c3451
jltasu <jltasu13@gmail.com>
2020-06-28 12:39:50 -0500

    diosa

--- src/Pages/principal.js
+++ src/Pages/principal.js
@@ -21,8 +21,8 @@ const Principal = () => {
                 <div className="container">
                   <div className="row align-items-center justify-content-center">
                     <div className="col-lg-6 text-center">
-                      <h1>We can make it together</h1>
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                      <h1>Descubre tu vocacion y potencial</h1>
+                      <p>Te acompañamos en el proceso, con el fin de darte la mejos visión para un futuro exitoso</p>
                     </div>
                   </div>
                 </div>
@@ -31,9 +31,9 @@ const Principal = () => {
             <div className="container site-section">
               <div className="row justify-content-center text-center mb-5">
                 <div className="col-md-6">
-                  <span className="text-primary">What We do</span>
-                  <h2 className="mb-4">The technology for the future generation</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis provident eius ratione velit, voluptas laborum nemo quas ad necessitatibus placeat?</p>
+                  <span className="text-primary">Explora tu vocación</span>
+                  <h2 className="mb-4">porque TÚ eres nuestra prioridad</h2>
+                  <p> Antes de empezar debes conocer los tres aspectos que te ayudaran a descubrir tu vocación</p>
                 </div>
               </div>
               <div className="row">
@@ -41,8 +41,11 @@ const Principal = () => {
                   <div className="feature-1">
                     <img src="images/img_1.jpg" alt="Image" className="img-fluid"></img>
                     <div className="feature-1-contents">
-                      <h2>Title Name Here</h2>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, officiis quo dicta molestias debitis.</p>
+                      <h2>INTERESES</h2>
+                      <p>Es la predisposición que sientes por realizar algunas actividades, ligadas a alguna ocupación o profesión, 
+                        en lugar de otras. Sin embargo, recuerda que solo puedes preferir algo si es que ya lo conoces. Por eso,
+                         debes explorar todas las opciones posibles antes de responder a la pregunta ¿Qué me interesa? Por ejemplo:
+                          ¿Qué carrera me interesa?</p>
                       <p><a href="#" className="more">Learn More</a></p>
                     </div>
                   </div>
@@ -51,8 +54,10 @@ const Principal = () => {
                   <div className="feature-1">
                     <img src="images/img_2.jpg" alt="Image" className="img-fluid"></img>
                     <div className="feature-1-contents">
-                      <h2>Title Name Here</h2>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, officiis quo dicta molestias debitis.</p>
+                      <h2>PREFERENCIAS Y HABILIDADES</h2>
+                      <p>Consiste en la selección de un determinado campo académico profesional compuesto de un conjunto de actividades
+                         lúdico-laborales que se quieran realidad profesionalmente y para el cual existen una serie de habilidades
+                          (aptitudes, conocimientos previos y motivos que llevan a una elección de titulación o profesión futura)</p>
                       <p><a href="#" className="more">Learn More</a></p>
                     </div>
                   </div>
@@ -61,8 +66,10 @@ const Principal = () => {
                   <div className="feature-1">
                     <img src="images/img_3.jpg" alt="Image" className="img-fluid"></img>
                     <div className="feature-1-contents">
-                      <h2>Title Name Here</h2>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, officiis quo dicta molestias debitis.</p>
+                      <h2>PERSONALIDAD</h2>
+                      <p>Cada uno de ustedes es único, por eso actúan, piensan y sienten de forma diferente. Tu personalidad es el conjunto
+                         de características que definirá quién eres. Piensa cuáles son los rasgos positivos de tu personalidad y aquellos
+                          que deberías mejorar; esto te ayudará a entender quién eres, para luego pensar quién quieres ser.</p>
                       <p><a href="#" className="more">Learn More</a></p>
                     </div>
                   </div>
@@ -74,8 +81,8 @@ const Principal = () => {
                 <div className="row justify-content-center text-center mb-5 section-2-title">
                   <div className="col-md-6">
                     <span className="text-primary">Our Features</span>
-                    <h2 className="mb-4">The technology for the future generation</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis provident eius ratione velit, voluptas laborum nemo quas ad necessitatibus placeat?</p>
+                    <h2 className="mb-4">¿Cómo funciona "Vocacional Test"?</h2>
+                    <p>solo debes seguir los siguientes pasos descritos a continuacion</p>
                   </div>
                 </div>
               </div>
@@ -87,9 +94,9 @@ const Principal = () => {
                   <div className="col-lg-3 feature-2-contents pl-lg-5">
                     <div className="fixed-content">
                       <span className="caption">01.</span>
-                      <h3 className="mb-5">Featured title name here</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus eaque laborum animi fugiat, suscipit in.</p>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est vitae magni impedit cum laudantium, voluptas.</p>
+                      <h3 className="mb-5">Descubre tu personalidad</h3>
+                      <p>Para descubrir tu vocación debes empezar por conocerte a ti mismo, así como entender qué es una vocación y qué no lo es.</p>
+                      <p>Registrarte con tu cuenta de facebook u otra red social con la que tengas mayor interacción</p>
                     </div>
                   </div>
                 </div>
@@ -102,9 +109,9 @@ const Principal = () => {
                   <div className="col-lg-3 feature-2-contents pr-lg-5">
                     <div className="fixed-content">
                       <span className="caption">02.</span>
-                      <h3 className="mb-5">Featured title name here</h3>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus eaque laborum animi fugiat, suscipit in.</p>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est vitae magni impedit cum laudantium, voluptas.</p>
+                      <h3 className="mb-5">Responde una serie de preguntas</h3>
+                      <p>En esta sección indicarás tus gustos y preferencias respecto de las actividades y profesiones que se presentan</p>
+                      <p>Responde la evaluación y obten las coincidencias de tu carrera y mucho más</p>
                     </div>
                   </div>
                 </div>
@@ -113,9 +120,9 @@ const Principal = () => {
             <div className="container site-section">
               <div className="row justify-content-center text-center mb-5">
                 <div className="col-md-6">
-                  <span className="text-primary">Design for the future</span>
-                  <h2 className="mb-4">The technology for the future generation</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis provident eius ratione velit, voluptas laborum nemo quas ad necessitatibus placeat?</p>
+                  <span className="text-primary">Descubre tu futuro</span>
+                  <h2 className="mb-4">Para cada etapa profesional, ten en cuenta lo siguiente</h2>
+                  <p>Te mostramos una breve descripción de cada etapa profesional, por las cuales atravesarás</p>
                 </div>
               </div>
               <div className="row justify-content-center">
@@ -123,26 +130,26 @@ const Principal = () => {
                   <div className="row">
                     <div className="col-lg-6 mr-auto pr-lg-5">
                       <div className="feature-3">
-                        <h3>Creative Technology</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores assumenda sequi magnam totam alias minima.</p>
+                        <h3>Profesionales que trabajan</h3>
+                        <p>Sé tu mejor yo en el trabajo. Aprenda qué lo hace único y qué tan adecuado es para sus elecciones de carrera pasadas, actuales y futuras.</p>
                       </div>
                     </div>
                     <div className="col-lg-6 ml-auto pl-lg-5">
                       <div className="feature-3">
-                        <h3>World-className Quality</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores assumenda sequi magnam totam alias minima.</p>
+                        <h3>Estudiantes universitarios y graduados</h3>
+                        <p>¿No estás seguro de qué hacer después de la universidad? Vea la variedad de carreras que puede seguir con sus intereses, personalidad y educación.</p>
                       </div>
                     </div>
                     <div className="col-lg-6 mr-auto pr-lg-5">
                       <div className="feature-3">
-                        <h3>Deep Resources</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores assumenda sequi magnam totam alias minima.</p>
+                        <h3>Cambiadores de carrera</h3>
+                        <p>¿Buscas hacer un cambio de carrera? ¿Pensando en volver a la escuela? CareerExplorer lo guiará en la dirección correcta.</p>
                       </div>
                     </div>
                     <div className="col-lg-6 ml-auto pl-lg-5">
                       <div className="feature-3">
-                        <h3>World-className Quality</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores assumenda sequi magnam totam alias minima.</p>
+                        <h3>Estudiantes de secundaria</h3>
+                        <p>Descubre tu verdadero potencial y todas las opciones que tienes después de la secundaria. Luego vea qué camino es el adecuado para usted.</p>
                       </div>
                     </div>
                   </div>
@@ -155,8 +162,8 @@ const Principal = () => {
                   <div className="col-md-7">
                     <blockquote className="testimonial-1">
                       <span className="quote quote-icon-wrap"><span className="icon-format_quote"></span></span>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus totam sit delectus earum facere ex ea sunt, eos?</p>
-                      <cite><span className="text-black">Mike Dorney</span> &mdash; <span className="text-muted">CEO and Co-Founder</span></cite>
+                      <p>Cuando debes hacer una eleccion y no la hacemos, esto ya es una elección</p>
+                      <cite><span className="text-black">William James</span> &mdash; <span className="text-muted">Filósofo y Psicólogo</span></cite>
                     </blockquote>
                   </div>
                 </div>
