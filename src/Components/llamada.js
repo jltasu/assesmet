fcf3289d03e3baa348fb8d85bbd0a88f7f5642bb
jltasu <jltasu13@gmail.com>
2020-08-03 10:07:10 -0500

    contact

---
+++ src/Components/llamada.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const VideoConference = () => {
+  const jitsiContainerId = "jitsi-container-id";
+  const [jitsi, setJitsi] = React.useState({});
+
+  const loadJitsiScript = () => {
+    let resolveLoadJitsiScriptPromise = null;
+
+    const loadJitsiScriptPromise = new Promise((resolve) => {
+      resolveLoadJitsiScriptPromise = resolve;
+    });
+
+    const script = document.createElement("script");
+    script.src = "https://meet.jit.si/external_api.js";
+    script.async = true;
+    script.onload = () => resolveLoadJitsiScriptPromise(true);
+    document.body.appendChild(script);
+
+    return loadJitsiScriptPromise;
+  };
+
+  const initialiseJitsi = async () => {
+    if (!window.JitsiMeetExternalAPI) {
+      await loadJitsiScript();
+    }
+    const options = {
+      roomName: "chavo",
+      parentNode: document.getElementById("jitsi-container-id"),
+      interfaceConfigOverwrite: {
+        filmStripOnly: false,
+        SHOW_JITSI_WATERMARK: true,
+        SHOW_CHROME_EXTENSION_BANNER: false,
+        SHOW_PROMOTIONAL_CLOSE_PAGE: false,
+      },
+      configOverwrite: {
+        disableSimulcast: false,
+      },
+    };
+    const _jitsi = new window.JitsiMeetExternalAPI("meet.jit.si", options);
+
+    setJitsi(_jitsi);
+  };
+
+  React.useEffect(() => {
+    initialiseJitsi();
+
+    return () => jitsi?.dispose?.();
+  }, []);
+
+  return <div style={{height: "760px" }} id={jitsiContainerId} />;
+};
+
+export default VideoConference;
